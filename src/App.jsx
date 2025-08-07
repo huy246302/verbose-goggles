57aa922b24ee24b693ad81bfd7c9d3165bfcbeb6
Nguyen Huy <huy246302@gmail.com>
2025-08-07 21:31:36 +0700

    add-nav-bar

--- src/App.jsx
+++ src/App.jsx
@@ -2,9 +2,12 @@ import { useState } from 'react';
 import './App.css';
 import { LoadingScreen } from './components/LoadingScreen';
 import "./index.css";
+import { Navbar } from './components/Navbar';
+import { MobileMenu } from './components/MobileMenu';
 
 function App() {
   const [isLoaded, setIsLoaded] = useState(false);
+  const [menuOpen, setMenuOpen] = useState(false);
 
   return (
     <>
@@ -16,7 +19,8 @@ function App() {
         ${isLoaded ? "opacity-100" : "opacity-0"}
         bg-black 
         text-gray-100`}>
-
+          <Navbar menuOpen={menuOpen} setMenuOpen={setMenuOpen}/>
+          <MobileMenu menuOpen={menuOpen} setMenuOpen={setMenuOpen}/>
       </div>
     </>
   )
